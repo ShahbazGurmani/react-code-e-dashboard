84e397bd177a5086af7cd4c1d8e736bafbc352ab
ShahbazGurmani <shahbazgurmani786@gmail.com>
2023-08-23 00:33:11 +0500

    first commit

--- .history/src/App_20230823001512.js
+++ .history/src/App_20230823001512.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <header className="App-header">
         <h1>E Dashboard</h1>
-      
+      </header>
     </div>
   );
 }
